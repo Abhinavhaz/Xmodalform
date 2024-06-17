e64a56760c450515fc993170d8b373aa2f945a98
Abhinav hazarika <abhinavhazarika27@gmail.com>
2024-06-17 11:15:08 +0530

    final

--- src/Components/Form/form.jsx
+++ src/Components/Form/form.jsx
@@ -1,42 +1,87 @@
+// import Modal from "../modal/modal";
+// import "../../App.css"
+// import { useState } from "react";
+// function Form(){
+// const [modal, setModal ] = useState(false)
+
+// const handleprev =()=>{
+// setModal(!modal)
+// }
+
+// const handleOutside = (e) => {
+//     if (e.target.className === "xyz") {
+//       setModal(false);
+//     }
+//   };
+// return(
+   
+// <>
+//     <div style={{textAlign:"center"}}  onClick={handleOutside}
+// className={modal &&("xyz")} >
+    
+// <h1>User Details Modal</h1>
+       
+
+//         <button style={{ backgroundColor:" rgb(54, 173, 252)",borderRadius:"10px" ,color:"white",height: "40px",width: "10%",border:0}}
+//         onClick={handleprev}
+        
+//         >Open Form</button>
+
+
+
+// {modal &&(
+//     <Modal setModal={setModal} />
+// )}
+//     </div>
+
+//     </>
+
+// )
+// }
+
+// export default  Form
+
+
 import Modal from "../modal/modal";
-import "../../App.css"
+import "../../App.css";
 import { useState } from "react";
-function Form(){
-const [modal, setModal ] = useState(false)
 
-const handleprev =()=>{
-setModal(!modal)
-}
+function Form() {
+  const [modal, setModal] = useState(false);
 
-const handleOutside = (e) => {
-    if (e.target.className === "xyz") {
+  const handlePrev = () => {
+    setModal(!modal);
+  };
+
+  const handleOutside = (e) => {
+    if (e.target.classList.contains("modal-container")) {
       setModal(false);
     }
   };
-return(
-   
-<>
-    <div style={{textAlign:"center"}}  onClick={handleOutside}
-className={modal &&("xyz")} >
-    
-<h1>User Details Modal</h1>
-       
 
-        <button style={{ backgroundColor:" rgb(54, 173, 252)",borderRadius:"10px" ,color:"white",height: "40px",width: "10%",border:0}}
-        onClick={handleprev}
-        
-        >Open Form</button>
-
-
-
-{modal &&(
-    <Modal setModal={setModal} />
-)}
+  return (
+    <div
+      style={{ marginTop: "-27px", textAlign: "center" }}
+      className={modal ? "modal-container" : ""}
+      onClick={handleOutside}
+    >
+      <h1>User Details Modal</h1>
+      <button
+        style={{
+          backgroundColor: "rgb(54, 173, 252)",
+          borderRadius: "10px",
+          color: "white",
+          height: "40px",
+          width: "10%",
+          border: 0,
+        }}
+        onClick={handlePrev}
+      >
+        Open Form
+      </button>
+      {modal && <Modal setModal={setModal} />}
     </div>
-
-    </>
-
-)
+  );
 }
 
-export default  Form
+export default Form;
